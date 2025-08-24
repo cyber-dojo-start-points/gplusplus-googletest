4a0fb2fc2cccb2d2739e3910564844a95a61963c
JonJagger <jon@kosli.com>
2025-08-24 09:46:19 +0100

    Add --gcov-ignore-parse-errors flag to gcovr call to avoid massive error log output

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -20,5 +20,4 @@ trap cyber_dojo_exit EXIT SIGTERM
 # --------------------------------------------------------------
 
 make
-# gcovr --root . > report/coverage.txt
-# Commented out line above to avoid unnecessary amount of output 
+gcovr --gcov-ignore-parse-errors --root . > report/coverage.txt
